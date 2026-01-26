36e71b393bb4cf4782c9bd148c30942421961939
csebille-ai <csebille@gmail.com>
2026-01-26 10:09:23 +0100

    Move mobile chat composer into bottom dock

--- resources/views/chat/index_v2.blade.php
+++ resources/views/chat/index_v2.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout pageBgClass="fam-page-bg">
+    <x-slot name="bottomDock">
+        <div class="sm:hidden border-t border-slate-100 bg-white">
+            @include('chat.partials.index-bottom-dock')
+        </div>
+    </x-slot>
+
     <div class="chat-theme flex flex-col min-h-[calc(100vh-var(--mobile-bottom-nav-h,4rem))]" data-chat-view="1">
         @include('chat.partials.index-header')
         @include('chat.partials.index-messages')
-
-        <div class="sm:hidden border-t border-slate-100 bg-white fixed left-0 right-0 bottom-[calc(var(--mobile-bottom-nav-h,4rem)+env(safe-area-inset-bottom))] z-[60]">
-            @include('chat.partials.index-bottom-dock')
-        </div>
 
         @include('chat.partials.index-composer-desktop')
         @include('chat.partials.overlays')
